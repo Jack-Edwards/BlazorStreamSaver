be015c0febd136ca5c45a5b4229d12aa49b31225
Jack-Edwards <jackedwards@protonmail.com>
2023-08-19 13:12:52 -0500

    wip

---
+++ BlazorStreamSaver/vite.config.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import terser from '@rollup/plugin-terser';
+import viteCompression from 'vite-plugin-compression';
+
+export default defineConfig({
+    build: {
+        lib: {
+            name: 'blazor-sodium',
+            entry: path.join(__dirname, 'Npm/src/blazorStreamSaver.js'),
+            fileName: (format) => `blazorStreamSaver.${format}.js`
+        },
+        rollupOutputOptions: {
+            plugins: [
+                terser({
+                    compress: {
+                        drop_console: false,
+                        passes: 3
+                    },
+                    mangle: true
+                }),
+                viteCompression({
+                    algorithm: 'brotliCompress',
+                    ext: 'br'
+                }),
+                viteCompression({
+                    algorithm: 'gzip',
+                    ext: 'gz',
+                    compressionOptions: {
+                        params: {
+                            quality: 11
+                        }
+                    }
+                })
+            ]
+        }
+    }
+});
